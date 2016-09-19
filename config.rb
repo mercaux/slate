5d85ad3fcb694db53aaaf8330f34e4a7489a58cc
Yuri Nikulin <unikulin@gmail.com>
2016-09-20 00:37:46 +0400

    Update config.rb

--- config.rb
+++ config.rb
@@ -50,17 +50,17 @@ activate :s3_sync do |s3_sync|
 end
 
 # Github pages require relative links
-activate :relative_assets
-set :relative_links, true
-#activate :asset_hash
-#activate :asset_host, :host => '//developer.mercaux.com'
+#activate :relative_assets
+#set :relative_links, true
+activate :asset_hash
+activate :asset_host, :host => '//developer.mercaux.com'
 
 # Build Configuration
 configure :build do
   # If you're having trouble with Middleman hanging, commenting
   # out the following two lines has been known to help
-  activate :minify_css
-  activate :minify_javascript
+  #activate :minify_css
+  #activate :minify_javascript
   # activate :relative_assets
   # activate :asset_hash
   # activate :gzip
